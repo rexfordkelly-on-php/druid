2aaa4a4b0c62a33af1ffc3eb818e7051b6fed936
Woodhouse <contact@icecave.com.au>
2013-10-13 23:06:26 +0000

    Publishing artifacts from build 6.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -2,8 +2,8 @@ var tree = [["Icecave",,"",
                 [["Druid","Icecave\/Druid.html","",
                 [["PackageInfo","Icecave\/Druid\/PackageInfo.html","",
                 [        ]],["Uuid","Icecave\/Druid\/Uuid.html","",
-                [        ]],["UuidFactoryInterface","Icecave\/Druid\/UuidFactoryInterface.html","",
+                [        ]],["UuidGeneratorInterface","Icecave\/Druid\/UuidGeneratorInterface.html","",
                 [        ]],["UuidInterface","Icecave\/Druid\/UuidInterface.html","",
-                [        ]],["UuidVersion4Factory","Icecave\/Druid\/UuidVersion4Factory.html","",
+                [        ]],["UuidVersion4Generator","Icecave\/Druid\/UuidVersion4Generator.html","",
                 [        ]]        ]]        ]]        ]
 
